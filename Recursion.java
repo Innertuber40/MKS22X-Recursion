c980c885625ae694aadc0ab55ab6eb0adab9dc3f
Innertuber <bstringham@stuy.edu>
2019-01-31 23:07:52 -0500

    Copy-pasted sqrt in, set up fib

---
+++ Recursion.java
@@ -0,0 +1,37 @@
+import Java.Math;
+
+public class Recursion {
+    public static double sqrt(double n, double tolerance) {
+        if (n == 0.0) {
+            return 0;
+        }
+        if (n < 0.0) {
+            throw new IllegalArgumentException("You can't take the square root of a negative number like " + n + '.');
+        }
+        return sqrtCompare(n, 1.0, tolerance / 100.0);
+    }
+    public static double sqrtCompare(double n, double g, double t) {
+        if (Math.abs(n - g*g) / n <= t) {
+            return g;
+        }
+        return sqrtCompare(n, (n/g + g) / 2, t);
+    }
+
+    public static int fib(int n) {
+        
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(sqrt(0, 0.000001));
+        System.out.println(sqrt(1, 0.000001));
+        System.out.println(sqrt(100, 0.000001));
+        System.out.println(sqrt(2, 0.000001));
+        System.out.println(sqrt(0.25, 0.000001));
+        try {
+            sqrt(-1, 0.000001);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+    }
+}
